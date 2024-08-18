eac269a99160055b647c477d92bd90f19f04b3c6
Yasin <37377965+yhreyes100@users.noreply.github.com>
2024-08-18 18:44:00 +0000

    m1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,121 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function src(typeofCard) {
+  let src = "";
+  switch (typeofCard) {
+    case 1:
+      src = "./src/assets/img/heart.png";
+      break;
+    case 2:
+      src = "./src/assets/img/clubs.png";
+      break;
+    case 3:
+      src = "./src/assets/img/rombo.png";
+      break;
+    case 4:
+      src = "./src/assets/img/picas.png";
+      break;
+    default:
+      src = "./src/assets/img/white.png";
+      break;
+  }
+  return src;
+}
+
+function createHeadFooter(cardNumber, typeofCard) {
+  let idtextcolor = "";
+  typeofCard % 2 == 0 ? (idtextcolor = "blackfont") : (idtextcolor = "redfont");
+  let number = "";
+  if (cardNumber == 1) number = "A";
+  else if (cardNumber > 1 && cardNumber < 10) number = cardNumber;
+  else if (cardNumber == 10) number = "J";
+  else if (cardNumber == 11) number = "Q";
+  else number = "K";
+  return (
+    "<div class='row'><div class='col-12'><b id='" +
+    idtextcolor +
+    "'>" +
+    number +
+    "</b></div></div><div class='row'><div class='col-2'><img class='img-fluid w-100 h-auto p-0 m-0' src='" +
+    src(typeofCard) +
+    "'/></div></div>"
+  );
+}
+
+function createBody(cardNumber, typeofCard) {
+  let result = "";
+  if (cardNumber > 1 && cardNumber < 10) {
+    let activeposition = {
+      "2": [
+        false,
+        true,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false
+      ],
+      "3": [false, true, false, false, true, false, false, true, false],
+      "4": [true, false, true, false, false, false, true, false, true],
+      "5": [true, false, true, false, true, false, true, false, true],
+      "6": [true, false, true, true, false, true, true, false, true],
+      "7": [true, false, true, true, true, true, true, false, true],
+      "8": [true, true, true, true, false, true, true, true, true],
+      "9": [true, true, true, true, true, true, true, true, true]
+    };
+    let pos = 0;
+    for (let i = 0; i < 3; i++) {
+      if (i != 2) result += "<div  class='row'>";
+      else result += "<div  class='row rotate'>";
+      for (let j = 0; j < 3; j++) {
+        console.log(pos);
+        if (activeposition[cardNumber][pos]) {
+          result +=
+            "<div class='col-4'> <img class='img-fluid w-100 h-auto p-0 m-0'  src='" +
+            src(typeofCard) +
+            "'/> </div>";
+        } else {
+          result +=
+            "<div class='col-4'> <img class='img-fluid w-100 h-auto p-0 m-0'  src='" +
+            src(100) +
+            "'/> </div>";
+        }
+
+        pos++;
+      }
+      result += "</div";
+    }
+  } else if (cardNumber == 1) {
+    result =
+      "<div class='row me-0'> <div class ='col-12 d-inline-flex justify-content-center' > <img id='lcard' src='" +
+      src(typeofCard) +
+      "'/> </div></div>";
+  }
+  return result;
+}
+function cardCreation(cardNumber = 1, typeofCard = 1) {
+  document.getElementsByClassName("card")[0].innerHTML +=
+    "<div class='cardhead'>" +
+    createHeadFooter(cardNumber, typeofCard) +
+    "</div>";
+  document.getElementsByClassName("card")[0].innerHTML +=
+    "<div class ='cardbody'>" + createBody(cardNumber, typeofCard) + "</div>";
+  document.getElementsByClassName("card")[0].innerHTML +=
+    "<div class='cardfooter rotate'>" +
+    createHeadFooter(cardNumber, typeofCard) +
+    "</div>";
+}
+function randomCards() {
+  return Math.floor(Math.random() * 9) + 1;
+}
+function randomTypes() {
+  return Math.floor(Math.random() * 4) + 1;
+}
 window.onload = function() {
-  //write your code here
+  cardCreation(randomCards(), randomTypes());
   console.log("Hello Rigo from the console!");
 };
